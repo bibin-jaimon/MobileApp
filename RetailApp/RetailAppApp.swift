0eed51f9f4d44a47a5d0861739ab8568619b8931
bibin-jaimon <bibinjaimon@gmail.com>
2023-05-18 13:47:24 +0530

    Initial Commit

---
+++ RetailApp/RetailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailAppApp.swift
+//  RetailApp
+//
+//  Created by Bibin Jaimon on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RetailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
